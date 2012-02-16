a05cd322c48e68b1946ffaeb7f14d4ca5d0b460d
Evan Tucker <etucker@mail.umw.edu>
2012-02-16 18:49:28 -0500

    Evan:Created database

---
+++ car-book.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE IF NOT EXISTS session;
+GRANT ALL PRIVILEGES ON session.* to 'assist'@'localhost' identified by 'assist';
+USE session;
+
+CREATE TABLE IF NOT EXISTS `users` (
+  
+);
+
+
+
+CREATE TABLE IF NOT EXISTS `cars` (
+  
+);
