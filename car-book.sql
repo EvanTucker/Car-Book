f129c0dfec039e92334c69fe993a778efa13bc30
kevinPatrickTyler <ktyler@umw.edu>
2012-02-23 22:17:54 -0500

    created user database

--- car-book.sql
+++ car-book.sql
@@ -1,12 +1,32 @@
-//Created by Evan Tucker
-
-CREATE DATABASE IF NOT EXISTS session;
-GRANT ALL PRIVILEGES ON session.* to 'assist'@'localhost' identified by 'assist';
-USE session;
+DROP DATABASE if exists carBook;
+CREATE DATABASE IF NOT EXISTS carBook;
+GRANT ALL PRIVILEGES ON carBook.* to 'cbConnect'@'localhost' identified by 'cbConnect';
+USE carBook;
 
 CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(12) NOT NULL,
+  `password` varchar(40) NOT NULL,
+  `firstName` varchar(20) NOT NULL,
+  `lastName` varchar(20) NOT NULL,
+  `email` varchar(20) NOT NULL,
+  `phoneNum` char(12),
+  `city` varchar(20),
+  `locState` char(2),
+  `vecType` char(1),
+  `buy` boolean,
+  `sell` boolean,
+  `yrsOwn` int(2),
+  `zipcode` int(5) NOT NULL,
+  PRIMARY KEY (`id`)
   
-);
+)ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1;
+
+
+INSERT INTO `users` (`id`, `username`, `password`, `firstName`, `lastName`, `email`,
+	`phoneNum`, `city`, `locState`, `vecType`, `buy`, `sell`, `yrsOwn`, `zipcode`) VALUES
+(1, 'ktyler', SHA('healthy'), 'Kevin', 'Tyler', 'celloguy009@yahoo.com', '757-331-4271', 'Richmond',
+'VA', 'N', 'TRUE', 'FALSE', 4, 23173);
 
 
 
