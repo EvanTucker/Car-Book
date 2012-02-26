07750ed23ac44e6096d98896f4cad6b515c3209e
kevinPatrickTyler <ktyler@umw.edu>
2012-02-26 16:41:44 -0500

    table update 3

--- car-book.sql
+++ car-book.sql
@@ -31,7 +31,6 @@ INSERT INTO `users` (`id`, `username`, `password`, `firstName`, `lastName`, `ema
 
 
 CREATE TABLE IF NOT EXISTS `cars` (
-=======
   `vin` CHAR(17) NOT NULL,
   `year_made` CHAR(4),
   `make` VARCHAR(20),
@@ -47,4 +46,3 @@ CREATE TABLE IF NOT EXISTS `cars` (
 );
 
 INSERT INTO `cars` (`vin`, `year_made`, `make`, `model`, `mileage`, `price`, `color`, `transmission`, `description`, `owner#`) VALUES('15787844787786473', '1999', 'Ford', 'Mustang', '143259', 23000, 'Green', '5 Speed Manual', 'blahblahblah', '757-331-4271'), ('123456789AII5699D', '1997', 'Lincoln', 'Towncar', '80000', '5700', 'Black', 'Automatic', ' ', '434-964-6743'), ('987654321GHD3465K', '2002', 'Toyota', 'Camry', '48000', 4500, 'Red', 'Automatic',  '  ', '540-344-6783'), ('8485683248348569HH', '2008', 'Jeep', 'Grand Cherokee', '60000', 7500, 'Green', '5 Speed Manual', ' ', '709-888-3722'), ('888AGHE23I456NN55', '2009', 'Pontiac', 'Grand Am', '100000', '6800', 'Blue', 'Automatic', ' ', '653-684-1124'), ('5G33J576NW21CC5H7', '1996', 'Izuzu', 'Trooper', '215000', 2300, 'Orange', '5 Speed Manual', ' ', '703-231-5821');
->>>>>>> Updated car table
