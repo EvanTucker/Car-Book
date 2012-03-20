d698b30d71c8be36b7e310cfeca003b481f738c6
Evan Tucker <etucker@mail.umw.edu>
2012-03-20 03:00:53 -0400

    revised addCarComplete.php

--- webPageFolder/addCarComplete.php
+++ webPageFolder/addCarComplete.php
@@ -18,20 +18,23 @@ session_start();
 <!-- CONTENT -->
 <h3>You have added a car for sale. </h3>
 <p></p>
-<label><a href="index.html" class="rLink" tabindex="5">Proceed to login page</a></label>
+<label><a href="home.php" class="rLink" tabindex="5">Return to main page.</a></label>
 <?php
 include('dbconnect.php');
+$id = $_SESSION['idNum'];
 $make = $_POST['make'];
 $model = $_POST['model'];
 $year = $_POST['year'];
 $mileage = $_POST['mileage'];
-$color = $_POST['ecolor'];
-$price = $_POST['phone'];
-$vin = $_POST['city'];
-$transmission = $_POST['transmission'];
+$color = $_POST['color'];
+$price = $_POST['price'];
+$vin = $_POST['vin'];
+$transmission = $_POST['trans'];
 
 $query = "INSERT INTO cars (make, model, year_made, mileage, color, price, vin, transmission) VALUES ('$make', '$model', '$year', '$mileage', '$color', '$price', '$vin', '$transmission')";
+$query2 = "INSERT INTO carOwnership (id, vin) VALUES ('$id', '$vin')";
 $result = mysqli_query($db, $query) or die("Error Querying Database");
+$result2 = mysqli_query($db, $query2) or die("Error Querying Database");
 
 mysqli_close($db);
 
